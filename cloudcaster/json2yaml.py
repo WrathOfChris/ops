d23409e27f70bf6811bd25663accf9524dce1436
Vincent Janelle <randomfrequency@gmail.com>
2014-07-15 14:29:57 -0500

    Add json to yaml conversion script

---
+++ cloudcaster/json2yaml.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import json
+import argparse
+import yaml
+from yaml import Loader, Dumper
+from pprint import pprint
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", "--verbose", help="verbosity", action="store_true")
+parser.add_argument("file", help="cloudcaster JSON file")
+args = parser.parse_args()
+
+if args.file == None:
+    parser.print_help()
+    sys.exit(1)
+
+verbose = args.verbose
+conffile = open(args.file).read()
+conf = json.loads(conffile)
+
+print yaml.safe_dump(conf)
+
